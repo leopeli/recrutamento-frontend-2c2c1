af674f597ffae34e51259761c14de818567faab4
leopeli <leopeli.it@gmail.com>
2019-07-22 22:42:39 -0300

    Criado o Header da pagina

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header/Header';
 import './components/Header/style.scss';
 
 const App = () => (
   <div className="App">
-    
+    <Header />
   </div>
 )
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+    <header id="header">
+        <img src="" alt=""/>
+        <h1>Data Clash</h1>
+    </header>
+);
+
+export default Header
